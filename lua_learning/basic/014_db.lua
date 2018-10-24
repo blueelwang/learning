259f35ebfaa899f1863aff8126385ee63f4ad24b
V g <327021593@qq.com>
2018-10-24 14:15:10 +0800

    修改db笔记

--- lua_learning/basic/014_db.lua
+++ lua_learning/basic/014_db.lua
@@ -3,16 +3,16 @@
 --- @version     2018-10-22 16:06:03
 ---
 
-require "luasql.mysql"
+local lm = require "luasql.mysql"
 
 --创建环境对象
-env = luasql.mysql()
+env = lm.mysql()
 
 --连接数据库
 conn = env:connect("tmp_test","root","...","23.236.77.86", 3306)
 
 --设置数据库的编码格式
-conn:execute"SET NAMES UTF8"
+conn:execute("SET NAMES UTF8")
 
 --执行数据库操作
 cur = conn:execute("select * from t1")
@@ -20,14 +20,9 @@ cur = conn:execute("select * from t1")
 row = cur:fetch({},"a")
 
 while row do
-    var = string.format("%s\n", row.a)
-
-    print(var)
-
+    print(string.format("%s", row.a))
     row = cur:fetch(row,"a")
 end
 
 conn:close()  --关闭数据库连接
 env:close()   --关闭数据库环境
-
-
