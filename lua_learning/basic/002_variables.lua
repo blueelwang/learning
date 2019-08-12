765f7882409629a8dc7e83d0e5af6e92bf204a76
jianwei18 <jianwei18@staff.weibo.com>
2019-08-12 10:00:40 +0800

    修改笔记

--- lua_learning/basic/002_variables.lua
+++ lua_learning/basic/002_variables.lua
@@ -1,4 +1,3 @@
-
 --变量在使用前，必须在代码中进行声明，即创建该变量。
 --编译程序执行代码之前编译器需要知道如何给语句变量开辟存储区，用于存储变量的值。
 --Lua 变量有三种类型：全局变量、局部变量、表中的域。
@@ -7,24 +6,32 @@
 --局部变量的作用域为从声明位置开始到所在语句块结束。
 --变量的默认值均为 nil。
 
----赋值语句
---Lua可以对多个变量同时赋值，变量列表和值列表的各个元素用逗号分开，赋值语句右边的值会依次赋给左边的变量。
-a, b = 10, 2*6
+
+a = 'www.daemoncoder.com'
+local b = 1
+
+print(a)
+
+
+--- 赋值语句
+-- Lua可以对多个变量同时赋值，变量列表和值列表的各个元素用逗号分开，赋值语句右边的值会依次赋给左边的变量。
+a, b = 10, 2 * 6
 --遇到赋值语句Lua会先计算右边所有的值然后再执行赋值操作，所以我们可以这样进行交换变量的值
 a, b = b, a
 --当变量个数和值的个数不一致时，Lua会一直以变量个数为基础采取以下策略：
 --1.变量个数 > 值的个数             按变量个数补足nil
 --2.变量个数 < 值的个数             多余的值会被忽略
-a,b,c,d = 1,2,3
-print(a,b,c,d)       --1	2	3	nil
-a,b,c = 4,3,2,1
-print(a,b,c,d)       --4	3	2	nil
+a, b, c, d = 1, 2, 3
+print(a, b, c, d) --1	2	3	nil
+a, b, c = 4, 3, 2, 1
+print(a, b, c, d) --4	3	2	nil
 --多值赋值经常用来交换变量，或将函数调用返回给变量：
 function f()
     return 'A', 'B'
 end
+
 a, b = f()
-print(a, b)         -- A	B
+print(a, b) -- A	B
 
 
 --应该尽可能的使用局部变量，有两个好处：
@@ -35,39 +42,40 @@ print(a, b)         -- A	B
 a = 3
 b = a
 a = 4
-print(b)                            --3
+print(b) --3
 a = nil
-print(b)                            --3
+print(b) --3
 -- 当我们为 table a 并设置元素，然后将 a 赋值给 b，则 a 与 b 都指向同一个内存。如果 a 设置为 nil ，则 b 同样能访问 table 的元素。
 -- 如果没有指定的变量指向a，Lua的垃圾回收机制会清理相对应的内存。
 --
-a = {key='value'}
+a = { key = 'value' }
 b = a
 for key, value in pairs(b) do
-    print(key, value)               --key value
+    print(key, value) --key value
 end
 
 a['key'] = 'value2'
 for key, value in pairs(b) do
-    print(key, value)               --key value2
+    print(key, value) --key value2
 end
 
-a = {key = 'value3'}
+a = { key = 'value3' }
 for key, value in pairs(b) do
-    print(key, value)               --key value2
+    print(key, value) --key value2
 end
 
-local function change_value (data)
+local function change_value(data)
     data.key = 'value4'
 end
+
 change_value(a)
 for key, value in pairs(b) do
-    print(key, value)               --key value2
+    print(key, value) --key value2
 end
 
 a = nil
 for key, value in pairs(b) do
-    print(key, value)               --key value2
+    print(key, value) --key value2
 end
 
 
--- lua_learning/basic/003_loop.lua
+++ lua_learning/basic/003_loop.lua
@@ -1,52 +1,92 @@
 
 --- while
-a=10
-while( a < 20 )
-do
-    print("a 的值为:", a)
-    a = a+1
+i = 0
+while i < 5 do      -- while 后面的括号可省略
+    print("i=" .. i)
+    i = i + 1
 end
 
-
+print('---------- 1 ----------')
 --- 数值for循环
-for i=10,19,2 do    -- 从10变化到19，每次+2
-    print(i)        -- 10 12 14 16 18
-end
-for i=10,1,-2 do    -- 从10变化到1，每次-2
-    print(i)        -- 10 8 6 4 2
-end
-for i=10,12,1 do    -- 从10变化到12，每次+1
+for i=10,12,1 do    -- 从10变化到12（包含10和12），每次+1
     print(i)        -- 10 11 12
 end
---步长可省，默认为1
-for i=10,12 do
+print('---------- 2 ----------')
+for i=10,9,1 do     -- 不会执行循环体
+    print(i)        -- 没有输出
+end
+print('---------- 2.1 ----------')
+for i=10,10,1 do
+    print(i)        -- 输出：10
+end
+print('---------- 3 ----------')
+for i=10,15,2 do    -- 从10变化到15，每次+2
+    print(i)        -- 10 12 14
+end
+print('---------- 4 ----------')
+for i=10,6,-2 do    -- 从10变化到6，每次-2
+    print(i)        -- 10 8 6
+end
+print('---------- 5 ----------')
+for i=10,7,-2 do    -- 从10变化到7，每次-2
+    print(i)        -- 10 8
+end
+print('---------- 6 ----------')
+for i=10,10,-2 do
+    print(i)        -- 10
+end
+print('---------- 7 ----------')
+for i=10,11,-2 do
+    print(i)        -- 没有输出
+end
+print('---------- 8 ----------')
+for i=10,12 do      -- 步长可省，默认为1
     print(i)        -- 10 11 12
 end
+print('---------- 9 ----------')
 --for的三个表达式在循环开始前一次性求值，以后不再进行求值。
---比如下面的f(x)只会在循环开始前执行一次，其结果用在后面的循环中
-function f(x)
-    print("function")
-    return x*2
+--比如下面的f(n)只会在循环开始前执行一次，其结果用在后面的循环中
+function f(n)
+    print("change number")
+    return n + 2
 end
-a = 3
-for a=1,f(a) do     -- 先根据全局变量a=3计算f(a), 再执行 for a=1,6 do ...
-    print(a)        -- 输出从1到6
+n = 3
+for n=1,f(n) do     -- 先根据全局变量n=3计算f(n), 再执行 for n=1,5 do ...
+    print(n)        -- 输出从1到5
 end
-print(a)            -- 输出3，for中产生的变量a是局部变量，不影响全局变量的值
+print(n)            -- 输出3，for中产生的变量n是局部变量，不影响全局变量的值
 
+print('---------- 10 ----------')
+arr = {'a', 'b', 'c'}
+for i, v in ipairs(arr) do
+   print(i, v)
+end
+--[[
+输出：
+1	a
+2	b
+3	c
+ ]]
+print('---------- 11 ----------')
 ---泛型for循环
-a = {"one", "two", "three"}
-for i, v in ipairs(a) do
+arr = {a="A", b="B", c="C"}
+for i, v in pairs(arr) do
     print(i, v)
 end
+--[[
+输出：
+b	B
+a	A
+c	C
+ ]]
 
-
+print('---------- 12 ----------')
 --- repeat util
-a = 10
+i = 0
 repeat
-    print("a的值为:", a)
-    a = a + 1
-until( a > 15 )
+    print("i=" .. i)
+    i = i + 1
+until i >= 5      -- 括号可省略
 
 
 --- break 语句
